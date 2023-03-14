2cd4c14f49e2a2e1798f8c0d825a69d6ff3bef8d
MementoMori2318 <johnrogerargarin@gmail.com>
2023-03-14 09:55:54 +0800

    hayss

--- IT2C_ITEC_Argarin/views/screens/WellcomeScreen.js
+++ IT2C_ITEC_Argarin/views/screens/WellcomeScreen.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Background from '../../components/Background';
+import Button from '../../components/Button';
+import { orange, white } from '../../components/Color';
 
-const WellcomeScreen = () => {
+const WellcomeScreen = ( { navigation } ) => {
+  const [userDetails, setUserDetails] = React.useState();
+  React.useEffect(() => {
+    getUserData();
+  }, []);
+  const getUserData = async () => {
+    const userData = await AsyncStorage.getItem("userData");
+
+    if (userData) {
+      console.log("Home Screen");
+      console.log(JSON.parse(userData));
+      setUserDetails(JSON.parse(userData));
+    }
+  };
+  const logout = () => {
+    AsyncStorage.setItem(
+      "userData",
+      JSON.stringify({ ...userDetails, loggedIn: false })
+    );
+
+    navigation.navigate("Home");
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Wellcome 🎉</Text>
+    <Background>
+    <View>
+     
+        <Text style={styles.wellcome}>Wellcome {userDetails?.username}🎉</Text>
+     
+      
     </View>
+    <Button bgColor={orange} txtcolor={white} title="Logout" onPress={logout}/>
+    </Background>
   );
 };
 
+const styles = StyleSheet.create({
+  wellcome: {
+    fontSize: 55, 
+    color: "#fff", 
+    fontWeight: "900", 
+    alignSelf: 'center',
+    paddingTop: 250,
+  },
+});
 export default WellcomeScreen;
