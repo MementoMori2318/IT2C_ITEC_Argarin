53bb54db24f370364d6b86c03a951b4312cf8e1b
MementoMori2318 <johnrogerargarin@gmail.com>
2023-03-20 19:07:05 +0800

    wew

--- IT2C_ITEC_Argarin/views/screens/Home.js
+++ IT2C_ITEC_Argarin/views/screens/Home.js
@@ -4,62 +4,64 @@ import Background from '../../components/Background';
 import Button from '../../components/Button';
 import { darkorange, orange, white, blue } from '../../components/Color';
 import 'expo-dev-client';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { GoogleSignin } from '@react-native-google-signin/google-signin';
+// import auth from '@react-native-firebase/auth';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = ({ navigation }) => {
-  GoogleSignin.configure({
-    webClientId: '231135650384-lbu9ueh0tui7p3n4sned9ulognsb9oej.apps.googleusercontent.com',
-  });
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = React.useState(true);
-  const [user, setUser] = React.useState();
-  const onGoogleButtonPress = async() => {
-    // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
+//   GoogleSignin.configure({
+//     webClientId: "231135650384-lbu9ueh0tui7p3n4sned9ulognsb9oej.apps.googleusercontent.com",
+//     offlineAccess: true
+//   });
+//   // Set an initializing state whilst Firebase connects
+//   const [initializing, setInitializing] = React.useState(true);
+//   const [user, setUser] = React.useState();
+
+//   const onGoogleButtonPress = async() => {
+//     // Check if your device supports Google Play
+//     await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+//     // Get the users ID token
+//     const { idToken } = await GoogleSignin.signIn();
   
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+//     // Create a Google credential with the token
+//     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
   
-    // Sign-in the user with the credential
-    //return auth().signInWithCredential(googleCredential);
-    const user_sign_in = auth().signInWithCredential(googleCredential);
-    user_sign_in.then((user) => {
-      console.log(user);
-      setUser(user);
-      GoogleLogedIn();
-    }).catch((error) => {
-      console.log(error);
-    })
-  };
+//     // Sign-in the user with the credential
+//     //return auth().signInWithCredential(googleCredential);
+//     const user_sign_in = auth().signInWithCredential(googleCredential);
+//     user_sign_in.then((user) => {
+//       console.log(user);
+//       setUser(user);
+//       GoogleLogedIn();
+//     }).catch((error) => {
+//       console.log(error);
+//     })
+//   };
    
+//    // Handle user state changes
+//    function onAuthStateChanged(user) {
+//      setUser(user);
+//      if (initializing) setInitializing(false);
+//    }
  
-   // Handle user state changes
-   function onAuthStateChanged(user) {
-     setUser(user);
-     if (initializing) setInitializing(false);
-   }
- 
-   React.useEffect(() => {
-     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-     return subscriber; // unsubscribe on unmount
-   }, []);
-   React.useEffect(() => {
-    if(user){
-      console.log(user);
-      GoogleLogedIn();
-     } else {
-      console.log("no user");
-     }
-  }, []);
+//    React.useEffect(() => {
+//      const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+//      return subscriber; // unsubscribe on unmount
+//    }, []);
+//    React.useEffect(() => {
+//     if(user){
+//       console.log(user);
+//       GoogleLogedIn();
+//      } else {
+//       console.log("no user");
+//      }
+//   }, []);
   
-const GoogleLogedIn = () => {
-  navigation.navigate("WelcomeScreen");
-  AsyncStorage.setItem("UserLogedInData", JSON.stringify({user, loggedIn: true}));
-}
+// const GoogleLogedIn = () => {
+//   navigation.navigate("WelcomeScreen");
+//   AsyncStorage.setItem("UserLogedInData", 
+//   JSON.stringify({user, loggedIn: true}));
+// }
   return (
     <Background >
       <View style={styles.container}>
--- IT2C_ITEC_Argarin/views/screens/WelcomeScreen.js
+++ IT2C_ITEC_Argarin/views/screens/WelcomeScreen.js
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Background from '../../components/Background';
 import Button from '../../components/Button';
 import { orange, white } from '../../components/Color';
-import auth from '@react-native-firebase/auth';
+//import auth from '@react-native-firebase/auth';
 
 const WelcomeScreen = ( { navigation } ) => {
   const [userDetails, setUserDetails] = React.useState();
@@ -13,7 +13,7 @@ const WelcomeScreen = ( { navigation } ) => {
   }, []);
   const getUserData = async () => {
     const userData = await AsyncStorage.getItem("userData");
-    const UserLogedInData = await AsyncStorage.getItem("UserLogedInData");   
+    // const UserLogedInData = await AsyncStorage.getItem("UserLogedInData");   
 
     if (userData) {
       console.log("Home Screen");
@@ -21,35 +21,33 @@ const WelcomeScreen = ( { navigation } ) => {
       setUserDetails(JSON.parse(userData));
     }
 
-    if (userData) {
+    // if (userData) {
       
-      console.log("UserLogedInData >>");
-      console.log(JSON.stringify(JSON.parse(UserLogedInData), null, 2));
-      console.log("UserLogedInData <<");
+    //   console.log("UserLogedInData >>");
+    //   console.log(JSON.stringify(JSON.parse(UserLogedInData), null, 2));
+    //   console.log("UserLogedInData <<");
 
-      var udata = JSON.parse(UserLogedInData);
-      console.log("udata >>");
-      console.log(udata.user.displayName);
-      console.log("udata <<");
-      setUserDetails(udata.user);
-    }
-  };
+    //   var udata = JSON.parse(UserLogedInData);
+    //   console.log("udata >>");
+    //   console.log(udata.user.displayName);
+    //   console.log("udata <<");
+    //   setUserDetails(udata.user);
+    // }
+   };
   const logout = () => {
     AsyncStorage.setItem(
       "userData",
       JSON.stringify({ ...userDetails, loggedIn: false })
     );
-    auth()
-    .signOut()
-    .then(() => console.log('User signed out!'));
+    // auth()
+    // .signOut()
+    // .then(() => console.log('User signed out!'));
     navigation.navigate("Home");
   };
   return (
     <Background>
     <View>
-        {/* {userDetails?.photoURL && (
-          <Image style={styles.image} source={{uri:userDetails?.photoURL}} />
-        )} */}
+    
         <Text style={styles.welcome}>Welcome {userDetails?.username}🎉</Text>
         <Text style={styles.welcome}>Welcome {userDetails?.displayName}🎉</Text>
      
